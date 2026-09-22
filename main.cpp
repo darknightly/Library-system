39d65b400c72f57730b3a7e51b7327f0894a6476
Dark <motazb000@gmail.com>
2026-09-22 20:23:52 +0200

    Auto-commit: 22-Sep-26 20:23:52.38

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+int main ()
+{
+    int test = 10 ; 
+    std::ifstream file("data.txt");
+    std::ofstream file("save.txt");
+    file << test ; 
+}
