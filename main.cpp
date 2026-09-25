2502428c038f2d93b5807be1f18f6eb9ee3270b1
Dark <motazb000@gmail.com>
2026-09-26 00:38:35 +0200

    Implement admin panel functionality and user authentication in library system

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+vector<string> books;
+vector<int> quantitiy;
+vector<int> bookid;
+bool panel = false ;
+int booki = 0;
+void adminpanel();
+void addbook()
+{
+    string ip;
+    cout << endl
+         << "Enter your book info: name , quantitiy: ";
+    cin >> ip;
+    books.push_back(ip);
+    int tity;
+    cin >> tity;
+    quantitiy.push_back(tity);
+    bookid[booki] = 100 + booki;
+    booki++;
+}
 void adminpanel()
-{   
-    int choice ; 
+{
+    int choice;
     cout << endl
          << "library Manu:" << endl
          << "1) add_book" << endl
@@ -20,38 +38,65 @@ void adminpanel()
          << "10) return to main manu" << endl
          << "11) Exit" << endl
          << endl
-         << "Enter your choice [1-10]: |";
-    cin >> choice ; 
-    switch (choice)
-    {
-        case 1 :
-
-    } 
+         << "Enter your choice [1-10]: |  ";
+    cin >> choice;
 
 }
-
-int main()
+void userpanel()
 {
-    cout << "===================" << "Welcome to the library system" << endl
-         << "===================" << endl
-         << "please Enter the user name";
+}
+string toupperforreal (string value )
+{   
+    for (int i =0 ; i < value.size() ; i++)
+    {
+        if (islower(value[i]))
+        {
+            toupper(value[i]) ; 
+        }
+    }
+    return value ; 
+}
+void startmenu()
+{
+    cout << "please Enter the your username : ";
     string user;
     vector<string> users{"motaz"};
     cin >> user;
-    if (user == "admin" || user == "Admin")
+    if (toupperforreal(user) == "ADMIN" )
     {
-        adminpanel() ; 
-
+        adminpanel();
     }
     else
     {
+        bool usercheck = false;
         for (int i = 0; i < users.size(); i++)
         {
             if (user == users[i])
             {
+                usercheck = true;
+                break;
             }
         }
+        if (usercheck)
+        {
+            userpanel;
+        }
+        else
+        {
+            cout << endl
+                 << "there is no user with this name" << endl
+                 << endl;
+            startmenu();
+        }
     }
+}
+
+int main()
+{
+    cout << "      ===================" << endl
+         << "Welcome to the library system" << endl
+         << "      ===================" << endl;
+    startmenu();
 
     return 0;
 }
